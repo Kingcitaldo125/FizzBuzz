5d960f11838a880565c09d9080c89d921d71669b
Paul <paularelt@gmail.com>
2021-02-01 17:35:41 -0500

    Update fizzbuzz.cpp
    Consolidation

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
 	for(unsigned int i = 1; i < 101; ++i)
 	{
-		if (i % 3 == 0)
-			cout << "Fizz" << endl;
-		else if (i % 5 == 0)
-			cout << "Buzz" << endl;
-		else
-			cout << i << endl;
+		cout << (i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : std::to_string(i)) << endl;
 	}
+	
 	return 0;
 }
