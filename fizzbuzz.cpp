83421eadc55c593d420b4499eb9d1eaec1fd2e6d
Paul <paularelt@gmail.com>
2021-02-14 15:05:13 -0500

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	for(unsigned int i = 1; i < 101; ++i)
 	{
-		cout << (i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : std::to_string(i)) << endl;
+		cout << (i % 3 == 0 && i % 5 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : std::to_string(i)) << endl;
 	}
 	
 	return 0;
