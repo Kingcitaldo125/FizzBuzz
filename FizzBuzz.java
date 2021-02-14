ffb9e3923e025ee744b94e1cb323e8c902c5c23c
Paul <paularelt@gmail.com>
2021-02-14 15:10:21 -0500

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,7 +4,14 @@ public class FizzBuzz
 	{
 		for(int i = 1; i < 101; i++)
 		{
-			System.out.println(i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i);
+			if(i % 3 == 0 && i % 5 == 0)
+				System.out.println("FizzBuzz");
+			else if (i % 3 == 0)
+				System.out.println("Fizz");
+			else if (i % 5 == 0)
+				System.out.println("Buzz");
+			else
+				System.out.println(i);
 		}
 	}
 }
